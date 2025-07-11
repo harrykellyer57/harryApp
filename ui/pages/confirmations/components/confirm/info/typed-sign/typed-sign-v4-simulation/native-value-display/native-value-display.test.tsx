7740cff5c427acad5d54652f4885acaa9a69b89f
harrykellyer57 <147956655+harrykellyer57@users.noreply.github.com>
2025-07-11 10:09:38 +0300

    Create native-value-display.test.tsx

---
+++ ui/pages/confirmations/components/confirm/info/typed-sign/typed-sign-v4-simulation/native-value-display/native-value-display.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+
+import mockState from '../../../../../../../../../test/data/mock-state.json';
+import { renderWithProvider } from '../../../../../../../../../test/lib/render-helpers';
+import NativeValueDisplay from './native-value-display';
+
+describe('NativeValueDisplay', () => {
+  it('renders component correctly', async () => {
+    const store = configureMockStore([])(mockState);
+    const { findByText } = renderWithProvider(<NativeValueDisplay value="4321" chainId="0x1" />, store);
+
+    await Promise.all([
+      expect(findByText('<0.000001')).resolves.toBeInTheDocument(),
+      expect(findByText('ETH')).resolves.toBeInTheDocument()
+    ]);
+  });
+});
